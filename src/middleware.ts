2abb9a128bf3470049d4abeba3cc311efc48e7dc
dailyworkcode <dailywork1433@gmail.com>
2026-03-15 18:59:28 +0530

    sleep module and food vision module

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,13 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 import createMiddleware from 'next-intl/middleware';
 import { locales, pathnames } from './navigation';
 
-const handleI18nRouting = createMiddleware({
-  // A list of all locales that are supported
+export default createMiddleware({
   locales,
   pathnames,
-
-  // Used when no locale matches
   defaultLocale: 'en',
   localePrefix: 'always'
 });
 
-export default function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  if (pathname === '/') {
-    return NextResponse.redirect(new URL('/en', request.url));
-  }
-
-  return handleI18nRouting(request);
-}
-
 export const config = {
-  // Match all pathnames except for
-  // - /api (API routes)
-  // - /_next (Next.js internals)
-  // - /_vercel (Vercel internals)
-  // - all files inside /public (e.g. favicon.ico)
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
+  matcher: ['/((?!api|_next|.*\\..*).*)']
 };
